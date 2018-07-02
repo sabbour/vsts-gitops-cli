10a6e3fbc7dcc4a6f98b35c281f4467911d4ae6d
Ahmed Sabbour <sabbour@outlook.com>
2018-07-02 12:37:26 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitops_cmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
