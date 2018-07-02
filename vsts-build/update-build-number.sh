fd57c224c079a5aad527b26e58be31e30dcd2c65
Ahmed Sabbour <sabbour@outlook.com>
2018-07-02 20:15:27 +0400

    Updated build number script

--- vsts-build/update-build-number.sh
+++ vsts-build/update-build-number.sh
@@ -3,14 +3,10 @@
 # Get a short Git commit ID
 short_commit_id=${BUILD_SOURCEVERSION:0:7}
 
-# If this is from a pull request, add the PR number, otherwise, don't
-if [ -z "$SYSTEM_PULLREQUEST_PULLREQUESTNUMBER"] 
-then
-  echo "not from pull request";
-  custom_build_tag=$BUILD_SOURCEBRANCHNAME-$BUILD_REASON-$short_commit_id-$BUILD_BUILDID
+if [ "$BUILD_REASON" = "PullRequest" ]; then
+  custom_build_tag=$BUILD_SOURCEBRANCHNAME-pr-$SYSTEM_PULLREQUEST_PULLREQUESTNUMBER-$short_commit_id-$BUILD_BUILDID
 else
-  echo "from pull request number $SYSTEM_PULLREQUEST_PULLREQUESTNUMBER";
-  custom_build_tag=$BUILD_SOURCEBRANCHNAME-$BUILD_REASON-$SYSTEM_PULLREQUEST_PULLREQUESTNUMBER-$short_commit_id-$BUILD_BUILDID
+  custom_build_tag=$BUILD_SOURCEBRANCHNAME-$short_commit_id-$BUILD_BUILDID
 fi
 
 # Convert to lowercase
