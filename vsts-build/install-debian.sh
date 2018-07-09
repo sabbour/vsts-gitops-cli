f4a779c5feed35fa1b41898373e433e4166d934a
Ahmed Sabbour <sabbour@outlook.com>
2018-07-09 17:12:05 +0400

    Added install script

---
+++ vsts-build/install-debian.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -Lo $(System.DefaultWorkingDirectory)/vsts-gitops-cli.zip https://aka.ms/vsts-gitops-debian-dev
+unzip -d $(System.DefaultWorkingDirectory)/vsts-gitops-cli $(System.DefaultWorkingDirectory)/vsts-gitops-cli.zip
+chmod +x $(System.DefaultWorkingDirectory)/vsts-gitops-cli/vsts-gitops-cli
+updated_path=$PATH:$(System.DefaultWorkingDirectory)/vsts-gitops-cli/
+echo "##vso[task.setvariable variable=PATH]$(echo $updated_path)"
