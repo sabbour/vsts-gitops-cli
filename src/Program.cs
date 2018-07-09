8a8f803995c7be262d02c16db87b26ea9a7e6859
Ahmed Sabbour <sabbour@outlook.com>
2018-07-09 16:03:58 +0400

    Updated env variable debug formatting

--- src/Program.cs
+++ src/Program.cs
@@ -19,10 +19,12 @@ namespace VSTSGitOps
 
         public static void PrintEnv() {
             var envVariables = System.Environment.GetEnvironmentVariables();
+            Console.WriteLine("*** BEGIN ENVIRONMENT VARIABLES ***");
             foreach (System.Collections.DictionaryEntry env in envVariables)
             {
                 Console.WriteLine($"{env.Key}: {env.Value}");
             }
+            Console.WriteLine("*** END ENVIRONMENT VARIABLES ***\n");
         }
     }
 }
