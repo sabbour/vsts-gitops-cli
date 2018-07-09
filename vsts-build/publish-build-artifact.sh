4286e4982b62989c3db9d84fb20916e211de533f
Ahmed Sabbour <sabbour@outlook.com>
2018-07-09 23:24:36 +0400

    Updated publish build artifact command

--- vsts-build/publish-build-artifact.sh
+++ vsts-build/publish-build-artifact.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Source: $1"
+echo "Destination: https://$2.blob.core.windows.net/$3/$4"
+
 azcopy \
     --source $1 \
     --destination https://$2.blob.core.windows.net/$3/$4 \
