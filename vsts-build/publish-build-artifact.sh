aad9b5f38fb8e3c886dee651f2c0acdb538eb6cf
Ahmed Sabbour <sabbour@outlook.com>
2018-07-09 23:28:31 +0400

    Added --verbose flag for azcopy

--- vsts-build/publish-build-artifact.sh
+++ vsts-build/publish-build-artifact.sh
@@ -3,4 +3,4 @@
 echo "Source: $1"
 echo "Destination: https://$2.blob.core.windows.net/$3/$4"
 
-azcopy --source $1 --destination https://$2.blob.core.windows.net/$3/$4 --dest-key $5
+azcopy --source $1 --destination https://$2.blob.core.windows.net/$3/$4 --dest-key $5 --verbose
